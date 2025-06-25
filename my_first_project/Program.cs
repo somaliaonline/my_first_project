3ff6613d3031590bdf1303308a577cc5fc94b949
somaliaonline <burhaanmaxamad61@gmail.com>
2025-06-25 20:28:38 +0300

    assignment

--- my_first_project/Program.cs
+++ my_first_project/Program.cs
@@ -1,5 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿string name = "abdullahi burhan abdi";
+Console.WriteLine(name);
 
+string motherName = "sucdi jama";
+Console.WriteLine(motherName);
 
-Console.WriteLine("hello");
+string dateOfBirth = "03/01/2003";
+Console.WriteLine(dateOfBirth);
 
+string placeOfBirth = "galkaio";
+Console.WriteLine(placeOfBirth);
+
+string address = "osss street Galkaio";
+Console.WriteLine(address);
+
+int telPhone = 9000092;
+Console.WriteLine(telPhone);
+
+string sex = "male";
+Console.WriteLine(sex);
+
+string status = "single";
+Console.WriteLine(status);
